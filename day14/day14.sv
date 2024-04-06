aa58f6cdcfc42c0898e1bac6621160a4baff8f2b
Dvir Hazut <53955899+Dvir1992@users.noreply.github.com>
2024-04-06 22:48:14 +0300

    Create day14.sv

---
+++ day14/day14.sv
@@ -0,0 +1,15 @@
+//fixed priority arbiter
+//designer: Dvir Hazut
+module day14#(parameter NUM_PORTS = 4)(
+     input       wire[NUM_PORTS-1:0] req_i,
+    output      wire[NUM_PORTS-1:0] gnt_o   // One-hot grant signal
+);
+  assign gnt_o[NUM_PORTS-1]=req_i[NUM_PORTS-1];
+  genvar i;
+  for(i=(NUM_PORTS-2);i>=0;i=i-1)begin
+    assign gnt_o[i]=(req_i[i])&(~(|(gnt_o[(NUM_PORTS-1):(i+1)])));
+   
+    
+  end
+
+endmodule
